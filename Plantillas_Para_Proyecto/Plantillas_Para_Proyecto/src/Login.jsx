bd96e59f0c7ca8a124d6d08d1c174eaac2b930a6
OmarYZY <omaramirez134@gmail.com>
2026-02-19 23:22:26 -0600

    Ejemplo de Login para el proyecto, tome en base el esqueleto de login que ya estaba y le di un formato de css

---
+++ Plantillas_Para_Proyecto/Plantillas_Para_Proyecto/src/Login.jsx
@@ -0,0 +1,46 @@
+import { useState } from "react";
+import "./Login.css"; // 👈 Importa el archivo
+
+function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("Email:", email);
+    console.log("Password:", password);
+  };
+
+  return (
+    <div className="login-container">
+      <h2>Login</h2>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label>Email:</label>
+          <input
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            required
+          />
+        </div>
+
+        <div>
+          <label>Contraseña:</label>
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+          />
+        </div>
+
+        <button type="submit">Ingresar</button>
+        <button type="button">Registrarse</button>
+      </form>
+    </div>
+  );
+}
+
+export default Login;
+
